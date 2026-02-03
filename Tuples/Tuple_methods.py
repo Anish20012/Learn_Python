92bed1ce3bb400d6d5cb124716c8feceddb3e230
Anish20012 <anish232001@gmail.com>
2026-02-03 15:56:01 +0530

    Add tuple methods practice files

---
+++ Tuples/Tuple_methods.py
@@ -0,0 +1,9 @@
+play_list = ('oru pere varalaru','God mode', 500,"Nallaru po","Kannukulla","Nallaru po")
+
+print(play_list)
+print(play_list[1])
+
+print(len(play_list))
+
+print(play_list.count("Nallaru po"))
+print(play_list.index("Nallaru po"))
